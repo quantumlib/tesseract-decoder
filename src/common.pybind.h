7acceb9e7a56139b99d55baca93ebd9fe2281c7c
Nour Yosri <noureldinyosri@google.com>
2025-06-10 19:58:29 -0700

    link tesseract with stim and add tests for the python wrapper of common.h

--- src/common.pybind.h
+++ src/common.pybind.h
@@ -1,41 +1,54 @@
 #ifndef TESSERACT_COMMON_PY_H
 #define TESSERACT_COMMON_PY_H
 
+#include <vector>
+
 #include <pybind11/operators.h>
 #include <pybind11/pybind11.h>
 #include <pybind11/stl.h>
 
-#include <vector>
+#include "src/stim/dem/dem_instruction.pybind.h"
+#include "stim/dem/detector_error_model_target.pybind.h"
 
 #include "common.h"
 
 namespace py = pybind11;
 
-void add_common_module(py::module &root) {
-  auto m = root.def_submodule("common", "classes commonly used by the decoder");
+void add_common_module(py::module &root)
+{
+    auto m = root.def_submodule("common", "classes commonly used by the decoder");
 
-  // TODO: add as_dem_instruction_targets
-  py::class_<common::Symptom>(m, "Symptom")
-      .def(py::init<std::vector<int>, common::ObservablesMask>(),
-           py::arg("detectors") = std::vector<int>(),
-           py::arg("observables") = 0)
-      .def_readwrite("detectors", &common::Symptom::detectors)
-      .def_readwrite("observables", &common::Symptom::observables)
-      .def("__str__", &common::Symptom::str)
-      .def(py::self == py::self)
-      .def(py::self != py::self);
+    py::class_<common::Symptom>(m, "Symptom")
+        .def(py::init<std::vector<int>, common::ObservablesMask>(),
+             py::arg("detectors") = std::vector<int>(),
+             py::arg("observables") = 0)
+        .def_readwrite("detectors", &common::Symptom::detectors)
+        .def_readwrite("observables", &common::Symptom::observables)
+        .def("__str__", &common::Symptom::str)
+        .def(py::self == py::self)
+        .def(py::self != py::self)
+        .def("as_dem_instruction_targets", [](common::Symptom s)
+             {
+            std::vector<stim_pybind::ExposedDemTarget> ret;
+            for(auto & t : s.as_dem_instruction_targets()) ret.emplace_back(t);
+            return ret; });
 
-  // TODO: add constructor with stim::DemInstruction.
-  py::class_<common::Error>(m, "Error")
-      .def_readwrite("likelihood_cost", &common::Error::likelihood_cost)
-      .def_readwrite("probability", &common::Error::probability)
-      .def_readwrite("symptom", &common::Error::symptom)
-      .def("__str__", &common::Error::str)
-      .def(py::init<>())
-      .def(py::init<double, std::vector<int> &, common::ObservablesMask,
-                    std::vector<bool> &>())
-      .def(py::init<double, double, std::vector<int> &, common::ObservablesMask,
-                    std::vector<bool> &>());
+    py::class_<common::Error>(m, "Error")
+        .def_readwrite("likelihood_cost", &common::Error::likelihood_cost)
+        .def_readwrite("probability", &common::Error::probability)
+        .def_readwrite("symptom", &common::Error::symptom)
+        .def("__str__", &common::Error::str)
+        .def(py::init<>())
+        .def(py::init<double, std::vector<int> &, common::ObservablesMask,
+                      std::vector<bool> &>())
+        .def(py::init<double, double, std::vector<int> &, common::ObservablesMask,
+                      std::vector<bool> &>())
+        .def(py::init([](stim_pybind::ExposedDemInstruction edi)
+                      { return new common::Error(edi.as_dem_instruction()); }));
+
+    m.def("merge_identical_errors", &common::merge_identical_errors);
+    m.def("remove_zero_probability_errors", &common::remove_zero_probability_errors);
+    m.def("dem_from_counts", &common::dem_from_counts);
 }
 
 #endif
