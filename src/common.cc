80afecabb98bf1e7bc12918a741f3cb1dd29e665
draganaurosgrbic <draganaurosgrbic@gmail.com>
2025-08-08 16:11:57 -0700

    Update src/common.cc
    Co-authored-by: oscarhiggott <29460323+oscarhiggott@users.noreply.github.com>

--- src/common.cc
+++ src/common.cc
@@ -25,6 +25,9 @@ std::string common::Symptom::str() {
 }
 
 common::Error::Error(const stim::DemInstruction& error) {
+  if (error.type != stim::DemInstructionType::DEM_ERROR) {
+     throw std::invalid_argument("Error must be loaded from an error dem instruction, but received: " + error.str());
+  }
   assert(error.type == stim::DemInstructionType::DEM_ERROR);
   double probability = error.arg_data[0];
   assert(probability >= 0 && probability <= 1);
