ec527713676d08ce6c19e414416faaca1813cf58
draganaurosgrbic <draganaurosgrbic@gmail.com>
2025-08-08 16:16:08 -0700

    Update src/common.cc
    Co-authored-by: oscarhiggott <29460323+oscarhiggott@users.noreply.github.com>

--- src/common.cc
+++ src/common.cc
@@ -95,7 +95,9 @@ stim::DetectorErrorModel common::merge_indistinguishable_errors(
     switch (instruction.type) {
       case stim::DemInstructionType::DEM_ERROR: {
         Error error(instruction);
-        assert(error.symptom.detectors.size());
+        if (error.symptom.detectors.size() == 0) {
+            throw std::invalid_argument("Errors that do not flip any detectors are not supported.");
+        }
         error.likelihood_cost =
             -1 * std::log(instruction.arg_data[0] / (1 - instruction.arg_data[0]));
 
